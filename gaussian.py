92ebb17a2215dba998ab13b9800e99c56b214894
Yan Aung Hein <yan@apmhelp.com>
2026-02-05 20:40:03 +0700

    test gaussian

--- gaussian.py
+++ gaussian.py
@@ -1,49 +1,33 @@
-A = [
-    [1, 1, 1],
-    [3, 2, 1],
-    [2, -1, 4]
-]
+A = [[1, 1, 1], [3, 2, 1], [2, -1, 4]]
 b = [6, 10, 12]
 
+n = len(A)
 
-def gaussian(A, b):
-    n = len(A)
-    # Forward pass of elimination
-    for i in range(n-1):
-        for j in range(i+1, n):
-            k_temp = - A[j][i] / A[i][i]
-            # print(f"Step {i+1} {j+1}: k = {k_temp:.2f}")
+# Forward pass of elimination
+for i in range(n-1):
+    for j in range(i+1, n):
+        k_temp = - A[j][i] / A[i][i]
+        print(f"Step {i+1} {j+1}: k = {k_temp:.2f}")
 
-            temp_row = [a * k_temp for a in A[i]]
-            # print(f"Temp row: {temp_row}")
+        temp_row = [a * k_temp for a in A[i]]
+        print(f"Temp row: {temp_row}")
 
-            for k in range(n):
-                A[j][k] += temp_row[k]
+        for k in range(n):
+            A[j][k] += temp_row[k]
 
-            b[j] += k_temp * b[i]
+        b[j] += k_temp * b[i]
 
-            # print(f"Updated A = {A}")
-            # print(f"Updated b = {b}")
+        print(f"Updated A = {A}")
+        print(f"Updated b = {b}")
 
-    x_sol = [None for _ in range(n)]
-    for i in range(n):
-        row_idx = n - i - 1
-        res = b[row_idx]
-        for j in range(n - i, n):
-            res -= A[row_idx][j] * x_sol[j]
-        x_sol[row_idx] = res / A[row_idx][row_idx]
+x_sol = [None for _ in range(n)]
+for i in range(n):
+    row_idx = n - i - 1
+    res = b[row_idx]
+    for j in range(n - i, n):
+        res -= A[row_idx][j] * x_sol[j]
+    x_sol[row_idx] = res / A[row_idx][row_idx]
 
-        print(f"x_{n-i-1} = {x_sol[n-i-1]:.2f}")
+    print(f"x_{n-i-1} = {x_sol[n-i-1]:.2f}")
 
-
-def generate(size=2):
-    matrix = [[0] * size for _ in range(size)]
-    for i in range(size):
-        for j in range(size):
-            matrix[i][j] = 1.0 / (i + j + 1)
-    return matrix
-
-
-if __name__ == '__main__':
-    matrix = generate(20)
-    gaussian(matrix, [1] * 20)
+print(f"Solution: {x_sol}")
